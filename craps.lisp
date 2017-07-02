ff5c05d66ce601d4466d535a3aab05d67f102425
joseph_scoggins <borninthecaul@gmail.com>
2017-07-02 11:10:55 -0700

    completes basic craps logic

--- craps.lisp
+++ craps.lisp
@@ -1,37 +1,68 @@
-(defun throwdice (n)
-  (setf sum 0)
+(setf craps-score 0)
+(setf craps-trial 0)
+(setf sum 0)
+
+(defun craps ()
   (setf roll 0)
+  (setf sum 0)
   (setf report '-)
-  (defun rolldie ()
-    (setf roll (+ (random 6) 1))
-    (setf sum (+ sum roll))
-    roll
-  )
-  (defun dumpdata (str)
-    (cond ((or (equal sum 7) (equal sum 11)) (setf report 'instant-win))
-          ((or (equal sum 12) (equal sum 2) (equal sum 3)) (setf report 'instant-loss))
-          (t 'nothing)
+
+  (defun throwdice (n)
+
+    (defun rolldie ()
+      (setf roll (+ (random 6) 1))
+      (setf sum (+ sum roll))
+      roll
     )
-    (list results str 'your-roll= sum report)
-  )
-  (if (> n 0) (setf result (list (rolldie))) nil)
-  (if (> n 1) (setf results (cons (rolldie) result)) nil)
-  (if (> n 2)
-    (loop for nextroll from 3 to n
-      do (setf results (cons (rolldie) results))
-    )
-    nil
-  )
-  (if (> n 0 )
-    (if (equal n 1) result
-      (if (equal n 2)
-        (cond ((and (equal (first results) 1) (equal (cadr results) 1)) (dumpdata 'snake-eyes))
-              ((and (equal (first results) 6) (equal (cadr results) 6)) (dumpdata 'boxcars))
-              (t (dumpdata '-))
+
+    (defun gameoverp ()
+      (if (not (equal report '-))
+        (cond ((equal report 'instant-win) (resetcraps 'win))
+              (t (resetcraps 'lose))
         )
-       (dumpdata '-)
+        (if (and (equal sum craps-score) (not (equal craps-trial 1))) (resetcraps 'win) '(call (craps) again to play on))
       )
     )
-   nil
+
+    (defun resetcraps (winloss)
+      (setf craps-score 0)
+      (setf craps-trial 0)
+      (list 'game 'over '- 'you winloss '- 'call '(craps) 'again 'to 'start 'over)
+    )
+
+    (defun dumpdata (str)
+      (if (equal craps-trial 0)
+        (cond ((or (equal sum 7) (equal sum 11)) (setf report 'instant-win))
+              ((or (equal sum 12) (equal sum 2) (equal sum 3)) (setf report 'instant-loss))
+              (t (setf craps-score sum))
+        )
+        (if (equal sum 7) (setf report 'game-over))
+      )
+      (incf craps-trial)
+      (list results str 'your-roll= sum report 'trials= craps-trial 'number-to-match= craps-score (gameoverp))
+    )
+
+    (if (> n 0) (setf result (list (rolldie))) nil)
+    (if (> n 1) (setf results (cons (rolldie) result)) nil)
+    (if (> n 2)
+      (loop for nextroll from 3 to n
+        do (setf results (cons (rolldie) results))
+      )
+      nil
+    )
+    (if (> n 0 )
+      (if (equal n 1) result
+        (if (equal n 2)
+          (cond ((and (equal (first results) 1) (equal (cadr results) 1)) (dumpdata 'snake-eyes))
+                ((and (equal (first results) 6) (equal (cadr results) 6)) (dumpdata 'boxcars))
+                (t (dumpdata '-))
+          )
+          (dumpdata '-)
+        )
+      )
+     nil
+    )
   )
+
+  (throwdice 2)
 )
