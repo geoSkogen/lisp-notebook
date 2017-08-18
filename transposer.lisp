90db61a2fcb38dfce7a2b00692eb213b1052ce5c
joseph_scoggins <borninthecaul@gmail.com>
2017-08-17 20:41:21 -0700

    adds modes to transposer

--- transposer.lisp
+++ transposer.lisp
@@ -1,3 +1,7 @@
+(setf modes
+  '(ionian dorian phrygian lydian mixolydian aeolian locrian)
+)
+
 (setf tonic
   '((one . do)
     (two . re)
@@ -23,37 +27,54 @@
     (12 . B))
 )
 
-(setf thischromatic nil)
+(setf thischromatic chromatic)
 (setf thisionian nil)
-(setf currentkey 'unset)
+(setf thismode nil)
+(setf currentkey 'c)
+(setf currentmode 'ionian)
 (setf coffset 0)
-(setf ioffset 0)
+(setf moffset 0)
 
+(defun indexof (thisin listin)
+ (if (member thisin listin)
+    (- (length listin) (length (member thisin listin :test #'equal)))
+    ()
+ )
+)
 
 (defun keychange (tonic)
+  (if (equal nil (rassoc tonic chromatic))
+    (format t "~A is not a known pitch." tonic)
+    (validkey tonic)
+  )
+)
+
+(defun validkey (tonic)
   (setf currentkey tonic)
   (setf coffset (- (car (rassoc tonic chromatic)) 1))
   (getkey)
 )
 
 (defun getkey ()
-  (if (equal currentkey 'unset) (routedata nil) (routetrans coffset))
+  (routetrans coffset)
 )
 
 (defun routetrans (coff)
   (setf thischromatic (transposer chromatic coff))
   (setf thisionian (buildionian thischromatic))
-  (routedata t)
-)
-
-(defun routedata (bool)
-  (if (equal t bool) (dumpdata thischromatic thisionian) (dumpdata chromatic ionian))
+  (setf thismode (modechange currentmode))
+  (dumpdata thischromatic thismode)
 )
 
 (defun dumpdata (alist1 alist2)
-  (format t "~A" alist1)
+  (format t "Key of ~A ~%" currentkey)
+  (format t "Chromatic Scale: ~A" alist1)
   (format t "~%" nil)
-  (format t "~A" alist2)
+  (if (not (equal currentmode 'ionian))
+    (format t "IONIAN mode: ~A ~%" thisionian)
+    (format t "~%" nil)
+  )
+  (format t "~A Mode: ~A" currentmode alist2)
 )
 
 (defun transposer (alist offset)
@@ -81,19 +102,22 @@
   ionian
 )
 
-;notice how this transposer is so much more awkward than the one above
-(defun *old*transposer ()
-  (setf thischromatic ())
-  (setf i 1)
-  (setf lookup i)
-  (setf thischromatic (cons (cons i currentkey) thischromatic))
-  (loop for elm from 1 to (- (length chromatic) 1)
-    do
-    (incf i)
-    (incf lookup)
-    (if (> lookup (- (length chromatic) offset)) (setf lookup (- lookup (length chromatic))))
-    (setf newcons (cons i (cdr (assoc (+ lookup offset) chromatic))))
-    (setf thischromatic (append thischromatic (list newcons)))
+(defun modechange (mode)
+  (if (numberp mode)
+    (if (> mode (length modes)) (format t "~A is out of range." mode)
+    (transmoder (nth (- mode 1) modes) thisionian))
+    (if (not (member mode modes)) (format t "~A is not a known mode." mode)
+      (transmoder mode thisionian)
+    )
   )
-  thischromatic
 )
+
+(defun transmoder (mode alist)
+  (setf moffset (indexof mode modes))
+  ;(setf hometone (cdr (nth homeindex alist)))
+  (setf currentmode mode)
+  (setf thismode (transposer alist moffset))
+  thismode
+)
+
+(getkey)
